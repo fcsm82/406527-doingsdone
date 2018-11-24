ca776ded99e75c50194e35bd7d51464726fb0b80
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2018-11-24 15:07:32 -0500

    1. Создание БД 2. Создание таблиц

--- schema.sql
+++ schema.sql
@@ -2,41 +2,34 @@ CREATE DATABASE doingsdone
  DEFAULT CHARACTER SET utf8
  DEFAULT COLLATE utf8_general_ci;
 
-CREATE TABLE `projects` (
-  `id` smallint(6) NOT NULL AUTO_INCREMENT,
-  `project` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
-  `user_id` mediumint(8) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `project` (`project`),
-  KEY `project-user` (`user_id`),
-  CONSTRAINT `project-user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-)
-
-CREATE TABLE `tasks` (
+USE doingsdone;
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `date_complete` timestamp NOT NULL,
-  `status` tinyint(1) DEFAULT '0',
-  `task` varchar(128) COLLATE utf8_bin DEFAULT NULL,
-  `file` varchar(128) COLLATE utf8_bin DEFAULT '',
-  `term` timestamp NULL DEFAULT NULL,
-  `user_id` mediumint(8) NOT NULL,
-  `project_id` smallint(6) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `date_complete` (`date_complete`,`status`,`term`),
-  KEY `task-user` (`user_id`),
-  KEY `task-project` (`project_id`),
-  CONSTRAINT `task-project` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`),
-  CONSTRAINT `task-user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-)
-
-CREATE TABLE `users` (
-  `id` mediumint(8) NOT NULL AUTO_INCREMENT,
-  `date_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `email` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `email` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '' UNIQUE,
   `name` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
   `password` char(64) COLLATE utf8_bin NOT NULL,
-  `task_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-)
+  PRIMARY KEY (`id`)
+);
+
+USE doingsdone;
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) CHARACTER SET utf8 DEFAULT NULL UNIQUE,
+  `user_id` mediumint(8) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+USE doingsdone;
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_complete` timestamp NOT NULL,
+  `status` tinyint(1) DEFAULT '0',
+  `name` varchar(128) DEFAULT NULL,
+  `file` varchar(128) COLLATE utf8_bin DEFAULT '',
+  `term` timestamp,
+  `user_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
