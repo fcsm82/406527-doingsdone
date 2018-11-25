3e23c4885978f792aff3adc0e6af796efedcd158
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2018-11-25 18:36:03 -0500

    1. Исправлены таблицы 2. Добавлены индексы 3. Добавлены внешние ключи

--- schema.sql
+++ schema.sql
@@ -1,38 +1,62 @@
 CREATE DATABASE doingsdone
- DEFAULT CHARACTER SET utf8
- DEFAULT COLLATE utf8_general_ci;
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
 
 USE doingsdone;
+
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `email` varchar(255) NOT NULL UNIQUE,
-  `name` varchar(255) NOT NULL,
-  `password` char(64) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY ind_email(email)
+                       `id` int(11) NOT NULL AUTO_INCREMENT,
+                       `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                       `email` varchar(255) NOT NULL UNIQUE,
+                       `name` varchar(255) NOT NULL,
+                       `password` varchar(255) NOT NULL,
+                       PRIMARY KEY (`id`)
 );
 
-USE doingsdone;
+
 CREATE TABLE `projects` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) UNIQUE,
-  `user_id` mediumint(8) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY ind_namel(name)
+                          `id` int(11) NOT NULL AUTO_INCREMENT,
+                          `name` varchar(64),
+                          `user_id` int(11) NOT NULL,
+                          PRIMARY KEY (`id`)
 );
 
-USE doingsdone;
+
 CREATE TABLE `tasks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `date_complete` timestamp,
-  `status` tinyint(1) DEFAULT '0',
-  `name` varchar(255),
-  `file` varchar(255),
-  `user_id` int(11) NOT NULL,
-  `project_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY ind_status(status),
-  KEY ind_datecomp(date_complete)
+                       `id` int(11) NOT NULL AUTO_INCREMENT,
+                       `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                       `complete_time` timestamp NULL,
+                       `term_time` timestamp NULL,
+                       `is_completed` tinyint(1) DEFAULT 0,
+                       `name` varchar(255),
+                       `file` varchar(255),
+                       `user_id` int(11) NOT NULL,
+                       `project_id` int(11) NOT NULL,
+                       PRIMARY KEY (`id`)
 );
+
+CREATE INDEX name_idx ON users(name);
+CREATE INDEX create_time_idx ON users(create_time);
+
+CREATE INDEX name_idx ON projects(name);
+
+CREATE INDEX create_time_idx ON tasks(create_time);
+CREATE INDEX complete_time_idx ON tasks(complete_time);
+CREATE INDEX term_time_idx ON tasks(term_time);
+CREATE INDEX is_completed_idx ON tasks(is_completed);
+
+ALTER TABLE projects
+  ADD FOREIGN KEY (user_id)
+    REFERENCES users(id);
+
+
+ALTER TABLE tasks
+  ADD FOREIGN KEY (user_id)
+    REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE tasks
+  ADD FOREIGN KEY (project_id)
+    REFERENCES projects(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE projects
+  ADD UNIQUE KEY user_id_name_udx (user_id, name);
