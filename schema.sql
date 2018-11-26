cb395be5c8a12f0aab6c346c5f04ab8489f6a037
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2018-11-26 12:13:21 -0500

    1. Исправлен порядок строчек кода 2. Форматирование кода

--- schema.sql
+++ schema.sql
@@ -1,54 +1,53 @@
+USE doingsdone;
 CREATE DATABASE doingsdone
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
 
-USE doingsdone;
-
 CREATE TABLE `users` (
-                       `id` int(11) NOT NULL AUTO_INCREMENT,
-                       `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                       `email` varchar(255) NOT NULL UNIQUE,
-                       `name` varchar(255) NOT NULL,
-                       `password` varchar(255) NOT NULL,
-                       PRIMARY KEY (`id`)
-);
-
-
-CREATE TABLE `projects` (
-                          `id` int(11) NOT NULL AUTO_INCREMENT,
-                          `name` varchar(64),
-                          `user_id` int(11) NOT NULL,
-                          PRIMARY KEY (`id`)
-);
-
-
-CREATE TABLE `tasks` (
-                       `id` int(11) NOT NULL AUTO_INCREMENT,
-                       `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                       `complete_time` timestamp NULL,
-                       `term_time` timestamp NULL,
-                       `is_completed` tinyint(1) DEFAULT 0,
-                       `name` varchar(255),
-                       `file` varchar(255),
-                       `user_id` int(11) NOT NULL,
-                       `project_id` int(11) NOT NULL,
-                       PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `email` varchar(255) NOT NULL UNIQUE,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
 CREATE INDEX name_idx ON users(name);
 CREATE INDEX create_time_idx ON users(create_time);
 
-CREATE INDEX name_idx ON projects(name);
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64),
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
-CREATE INDEX create_time_idx ON tasks(create_time);
-CREATE INDEX complete_time_idx ON tasks(complete_time);
-CREATE INDEX term_time_idx ON tasks(term_time);
-CREATE INDEX is_completed_idx ON tasks(is_completed);
+CREATE INDEX name_idx ON projects(name);
 
 ALTER TABLE projects
   ADD FOREIGN KEY (user_id)
     REFERENCES users(id);
 
+ALTER TABLE projects
+  ADD UNIQUE KEY user_id_name_udx (user_id, name);
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `complete_time` timestamp NULL,
+  `term_time` timestamp NULL,
+  `is_completed` tinyint(1) DEFAULT 0,
+  `name` varchar(255),
+  `file` varchar(255),
+  `user_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE INDEX create_time_idx ON tasks(create_time);
+CREATE INDEX complete_time_idx ON tasks(complete_time);
+CREATE INDEX term_time_idx ON tasks(term_time);
+CREATE INDEX is_completed_idx ON tasks(is_completed);
 
 ALTER TABLE tasks
   ADD FOREIGN KEY (user_id)
@@ -57,6 +56,3 @@ ALTER TABLE tasks
 ALTER TABLE tasks
   ADD FOREIGN KEY (project_id)
     REFERENCES projects(id) ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE projects
-  ADD UNIQUE KEY user_id_name_udx (user_id, name);
