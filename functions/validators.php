908b835d78669a2c036bfac80c9cb6da26918792
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2018-12-11 07:03:28 -0500

    #13 Получение данных из форм 1. Исправлена валидация поля  "файл" 2. Исправлены описания функций

--- functions/validators.php
+++ functions/validators.php
@@ -3,6 +3,7 @@ const ERROR_EMPTY_FIELD = 'Обязательное к заполнению по
 const ERROR_LENGTH_FIELD = 'Количество символов в поле должно быть ';
 const ERROR_PROJECT_ID = 'Необходимо выбрать проект';
 const ERROR_DATE_FIELD = 'Введите дату в формате ДД.ММ.ГГГГ';
+const ERROR_EMAIL_EXIST = 'Пользователь с указанным адресом эл. почты уже зарегистрирован';
 
 /**
  * Функция валидации формы
@@ -17,7 +18,11 @@ function validateTaskForm($task_data, $connection)
     $results['name'] = validateName($task_data['name']);
     $results['project'] = validateProject($task_data['project'], $connection);
     $results['date'] = validateCompletionDate($task_data['date']);
-    $results['preview'] = validateAttachment($task_data['file_name'], $task_data['file_tmp_name']);
+
+    if (isset($file_tmp_name)) {
+        $results['preview'] = validateAttachment($task_data['file_name'], $task_data['file_tmp_name']);
+    }
+
 
     $errors = getErrors($results);
 
@@ -26,7 +31,7 @@ function validateTaskForm($task_data, $connection)
 
 /**
  * Функция валидации поля формы "Название задачи"
- * @param array $name данные из массива $_POST
+ * @param string $name данные из массива $_POST
  * @return bool|string Возращает true или текст ошибки
  */
 function validateName($name)
@@ -74,7 +79,7 @@ function validateCompletionDate($input_date) {
 
 /**
  * Функция проверки длины строки
- * @param str $string текстовая строка
+ * @param string $string текстовая строка
  * @param int $min Минимальное количество символов в строке
  * @param int $max Максимальное количество символов в строке
  * @return bool|string Возращает true или текст ошибки
@@ -89,17 +94,67 @@ function checkLength($string, $min, $max) {
 
 /**
  * Фнукция валидации поля "Файл"
- * @param str $file_name имя файла из массива $_FILES
+ * @param string $file_name имя файла из массива $_FILES
  * @param $file_tmp_name временное имя файла из массива $_FILES
  * @return bool Возращает true
  */
 function validateAttachment($file_name, $file_tmp_name) {
-    if (isset($file_tmp_name)) {
-        $target = APP_DIR .'/'. basename($file_name);
-        $t = move_uploaded_file($file_tmp_name, $target);
+    $target = APP_DIR .'/'. basename($file_name);
+    $t = move_uploaded_file($file_tmp_name, $target);
+    return true;
+}
+
+/**
+ * Функция валидации формы
+ * @param array $reg_data Массив $_POST
+ * @param mysqli object $connection Объект подключения к БД
+ * @return array|bool Возращает true или массив с ошибками
+ */
+function validateRegForm($reg_data, $connection)
+{
+    $results = [];
+
+    $results['email'] = validateEmail($reg_data['email'], $connection);
+    $results['password'] = validatePassword($reg_data['password']);
+    $results['name'] = validateName($reg_data['name']);
+
+    $errors = getErrors($results);
+
+    return $errors ? $errors : true;
+}
+
+function validateEmail($email, $connection)
+{
+    if (empty($email)) {
+        return ERROR_EMPTY_FIELD;
+    }
+    $id = getIdByEmail($email, $connection);
+
+    if (!empty($id)) {
+        return ERROR_EMAIL_EXIST;
+    }
+
+    $check_length = checkLength($email, 1, 255);
+    if ($check_length !== true) {
+        return $check_length;
     }
     return true;
 }
+
+function validatePassword($password)
+{
+    if (empty($password)) {
+        return ERROR_EMPTY_FIELD;
+    }
+    $check_length = checkLength($password, 6, 255);
+    if ($check_length !== true) {
+        return $check_length;
+    }
+
+    return true;
+}
+
+
 
 /**
  * Функция агреирования ошибок валидации формы
