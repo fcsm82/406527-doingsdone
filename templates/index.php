04d9fcf94bcf1c03c4c52eb37252e7e6adbdfdce
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-10-23 19:51:13 -0400

    feat: translated index.php

--- templates/index.php
+++ templates/index.php
@@ -1,21 +1,20 @@
-<h2 class="content__main-heading">Список задач</h2>
+<h2 class="content__main-heading">List of tasks</h2>
 
 <form class="search-form" action="index.php" method="get">
-    <input class="search-form__input" type="text" name="search" value="" placeholder="Поиск по задачам">
-    <input class="search-form__submit" type="submit" name="" value="Искать">
+    <input class="search-form__input" type="text" name="search" value="" placeholder="Search by tasks">
+    <input class="search-form__submit" type="submit" name="" value="Search">
 </form>
 
 <div class="tasks-controls">
     <nav class="tasks-switch">
         <a href="<?= buildUrlForFilter(); ?>"
-           class="tasks-switch__item <?= !isset($filter) || $filter === 'all' ? 'tasks-switch__item--active' : '' ?>">Все
-            задачи</a>
+           class="tasks-switch__item <?= !isset($filter) || $filter === 'all' ? 'tasks-switch__item--active' : '' ?>">All tasks</a>
         <a href="<?= buildUrlForFilter('today'); ?>"
-           class="tasks-switch__item <?= $filter === 'today' ? 'tasks-switch__item--active' : '' ?>">Повестка дня</a>
+           class="tasks-switch__item <?= $filter === 'today' ? 'tasks-switch__item--active' : '' ?>">Agenda</a>
         <a href="<?= buildUrlForFilter('tomorrow'); ?>"
-           class="tasks-switch__item <?= $filter === 'tomorrow' ? 'tasks-switch__item--active' : '' ?>">Завтра</a>
+           class="tasks-switch__item <?= $filter === 'tomorrow' ? 'tasks-switch__item--active' : '' ?>">Tomorrow</a>
         <a href="<?= buildUrlForFilter('overdue'); ?>"
-           class="tasks-switch__item <?= $filter === 'overdue' ? 'tasks-switch__item--active' : '' ?>">Просроченные</a>
+           class="tasks-switch__item <?= $filter === 'overdue' ? 'tasks-switch__item--active' : '' ?>">Overdue</a>
     </nav>
 
     <label class="checkbox">
@@ -23,7 +22,7 @@
             <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
             <input class="checkbox__input visually-hidden show_completed"
                    type="checkbox" <?= ($show_complete_tasks === '1') ? 'checked' : ''; ?>>
-            <span class="checkbox__text">Показывать выполненные</span>
+            <span class="checkbox__text">Show completed</span>
         </a>
     </label>
 </div>
