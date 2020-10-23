405da5eace892d024b77ed7ae15ad6c10284a913
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-10-23 19:23:50 -0400

    feat: translated add_project.php

--- templates/add_project.php
+++ templates/add_project.php
@@ -1,14 +1,14 @@
 <main class="content__main">
-    <h2 class="content__main-heading">Добавление проекта</h2>
+    <h2 class="content__main-heading">Add Project</h2>
 
     <form class="form" action="add_project.php" method="post">
         <div class="form__row">
             <?php $classname = isset($errors['name']) ? 'form__input--error' : '';
             $value = $project_data['name'] ?? ''; ?>
 
-            <label class="form__label" for="project_name">Название <sup>*</sup></label>
+            <label class="form__label" for="project_name">Name <sup>*</sup></label>
             <input class="form__input <?= $classname; ?>" type="text" name="name" id="project_name"
-                   value="<?= $value; ?>" placeholder="Введите название проекта">
+                   value="<?= $value; ?>" placeholder="Enter project's name">
 
             <?php if (isset($errors['name'])) : ?>
                 <p class="form__message"><?= $errors['name']; ?></p>
@@ -16,7 +16,7 @@
         </div>
 
         <div class="form__row form__row--controls">
-            <input class="button" type="submit" name="" value="Добавить">
+            <input class="button" type="submit" name="" value="Add">
         </div>
     </form>
 </main>
