5e46ad14fd866d1921a0c1166b7a1967e40bdc76
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-10-24 18:21:35 -0400

    feat: translated change-status.php

--- change-status.php
+++ change-status.php
@@ -2,10 +2,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Задаем текущую директорию
+// Set the current directory
 const APP_DIR = __DIR__;
 
-// Подключаем файлы с функциями
+// Connect files with functions
 require_once APP_DIR . '/functions/database.php';
 require_once APP_DIR . '/functions/functions.php';
 require_once APP_DIR . '/functions/get_tasks.php';
@@ -17,9 +17,9 @@ require_once APP_DIR . '/functions/url.php';
 if (!file_exists(APP_DIR . '/config.php')) {
     die('На основе config.sample.php создайте файл config.php, указав в нём настройки для подключениия к БД');
 }
-// Подключаем файл с настройками
+// Connect the file with settings
 $config = require APP_DIR . '/config.php';
-// Подключаемся к БД
+// Connect to the database
 $connection = dbConnect($config['db']);
 
 if (isset($_GET['task_id'])) {
